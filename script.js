1276644b7fe66547a37c76862e459d0138890bb7
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2021-11-17 20:58:30 -0600

    added current day display function to js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Javascript Page
+
+let currentDate = $("#currentDay");
+
+
+
+
+
+
+
+
+
+
+
+function displayTime() {
+    let rightNow = moment().format("dddd");
+    currentDate.text(rightNow);
+  }
+
+setInterval(displayTime, 1000)
