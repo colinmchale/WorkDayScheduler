d1fbcc6d215f1fe2047a75d9feacc7bb1b427490
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2021-11-18 18:49:30 -0600

    started button to save items to local storage

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
 // Javascript Page
 
 let currentDate = $("#currentDay");
+let description = $(".description");
+let saveBtn = $(".saveBtn")
 
 
 
+function renderLastAppointment() {
+    let appointment = localStorage.getItem("appointment");
+  
+    if (!appointment) {
+      return;
+    }
+  
+    description.textContent = appointment;
+  }
 
 
+saveBtn.on("click", function(event) {
+    event.preventDefault();
+    let $target = $(event.target)
+if ($target.is("i")) {
+    $target = $target.parent()
+}
+
+    let $description = $target.siblings("textarea")
+    console.log($description.val());
+
+    console.log($target.data.value);
+
+    timeSlot = $target.value
 
 
-
-
+    let appointment = description.value;
+  console.log(event.target)
+      localStorage.setItem("appointment", $description.val());
+      renderLastAppointment();
+    });
 
 
 function displayTime() {
-    let rightNow = moment().format("LLLL");
+    let rightNow = moment().format("dddd, MMMM Do");
     currentDate.text(rightNow);
   }
 
