47de97e16ced5d5b5f3b7edf89fe42a8c05cce3a
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2021-11-18 20:09:34 -0600

    added function to change color of description box depending in time in js

--- script.js
+++ script.js
@@ -2,19 +2,52 @@
 
 let currentDate = $("#currentDay");
 let description = $(".description");
-let saveBtn = $(".saveBtn")
+let saveBtn = $(".saveBtn");
+let currentHour = moment().format("H")
+
+let hourDescription = [];
 
 
 
-function renderLastAppointment() {
-    let appointment = localStorage.getItem("appointment");
+function renderDescription() {
+    hourDescription = localStorage.getItem("description");
+    hourDescription = JSON.parse(hourDescription);
+
+    for (let i = 0; i < hourDescription.length; i++) {
+        let itemHour = hourDescription[i].hour;
+        let itemText = hourDescription[i].text;
+
+        $("[data-hour=" + itemHour + "]").children("textarea").val(itemText);        
+    }
+
+
+
+    let timeDescription = localStorage.getItem("description");
   
-    if (!appointment) {
+    if (!timeDescription) {
       return;
     }
   
-    description.textContent = appointment;
+    description.textContent = timeDescription;
   }
+
+
+function hourColor() {
+    description.each(function(){
+        let thisDescription = $(this)
+        let thisHour = parseInt(thisDescription.attr("data-hour"));
+
+        if (thisHour == currentHour) {
+            thisDescription.addClass("present").removeClass("past future")
+        } else if (thisHour > currentHour) {
+            thisDescription.addClass("future").removeClass("past present")
+        } else {
+            thisDescription.addClass("past").removeClass("present future")
+        }
+    })
+};
+
+
 
 
 saveBtn.on("click", function(event) {
@@ -27,15 +60,11 @@ if ($target.is("i")) {
     let $description = $target.siblings("textarea")
     console.log($description.val());
 
-    console.log($target.data.value);
-
     timeSlot = $target.value
 
-
-    let appointment = description.value;
   console.log(event.target)
-      localStorage.setItem("appointment", $description.val());
-      renderLastAppointment();
+      localStorage.setItem("description", $description.val());
+      renderDescription();
     });
 
 
@@ -43,5 +72,7 @@ function displayTime() {
     let rightNow = moment().format("dddd, MMMM Do");
     currentDate.text(rightNow);
   }
+
+hourColor()
 
 setInterval(displayTime, 1000)
